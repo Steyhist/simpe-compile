7fbd128847f664514b8683a41a8450218f0ea256
Stey Histeria <153775773+Steyhist@users.noreply.github.com>
2023-12-24 14:49:48 +0700

    Yuki Clang

--- compile_with_yc.sh
+++ compile_with_yc.sh
@@ -1,10 +1,47 @@
-    PATH="$HOME/workspace/Kernel_tree/yuki-clang/bin:${PATH}" \
-      make -j$(nproc --all) \
+#!/bin/bash
+
+# setup color
+red='\033[0;31m'
+green='\e[0;32m'
+white='\033[0m'
+yellow='\033[0;33m'
+
+# setup dir
+WORK_DIR=$(pwd)
+KERN_IMG="${WORK_DIR}/out/arch/arm64/boot/Image-gz.dtb"
+KERN_IMG2="${WORK_DIR}/out/arch/arm64/boot/Image.gz"
+
+function clean() {
+    echo -e "\n"
+    echo -e "$red << cleaning up >> \n$white"
+    echo -e "\n"
+    rm -rf out
+}
+
+function build_kernel() {
+     export PATH="$HOME/workspace/Kernel_tree/yuki-clang/bin:${PATH}"
+      make -j$(nproc --all) O=out ARCH=arm64 <DEVICE>_defconfig
+      make -j$(nproc --all) ARCH=arm64 O=out \
       ARCH=arm64 \
       LD=ld.lld \
       NM=llvm-nm \
       AR=llvm-ar \
-      CC="ccache clang" \
+      CC="clang" \
       CLANG_TRIPLE=aarch64-linux-gnu- \
       CROSS_COMPILE=aarch64-linux-gnu- \
       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+    if [ -e "$KERN_IMG" ] || [ -e "$KERN_IMG2" ]; then
+        echo -e "\n"
+        echo -e "$green << compile kernel success! >> \n$white"
+        echo -e "\n"
+    else
+        echo -e "\n"
+        echo -e "$red << compile kernel failed! >> \n$white"
+        echo -e "\n"
+    fi
+}
+
+# execute
+clean
+build_kernel
